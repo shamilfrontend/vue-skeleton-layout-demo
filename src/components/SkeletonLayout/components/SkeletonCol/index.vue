abba717a3efe380440a3e231896abada0ba7bbc9
Shamil Frontend <sh.alisultanov@maximatelecom.ru>
2020-03-05 14:09:14 +0300

    first commit

---
+++ src/components/SkeletonLayout/components/SkeletonCol/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="skeleton-col">
+    skeleton-col
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SkeletonCol',
+};
+</script>
+
+<style lang="scss" scoped>
+.skeleton-col {
+}
+</style>
---
+++ src/components/SkeletonLayout/components/SkeletonRow/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="skeleton-row">
+    skeleton-col
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SkeletonRow',
+};
+</script>
+
+<style lang="scss" scoped>
+.skeleton-row {
+}
+</style>
